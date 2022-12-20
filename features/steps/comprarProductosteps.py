e12029166331f0cf91c0d86db28a1617f8a87276
Miguel Miranda <miguel.miranda@tiendamia.com>
2022-12-19 22:51:25 -0600

    fix in methods names

--- features/steps/comprarProductosteps.py
+++ features/steps/comprarProductosteps.py
@@ -5,19 +5,19 @@ stat = False
 
 
 @given('El usuario inicia el navegador')
-def launchBrowser(context):
+def launchbrowser(context):
     context.driver = webdriver.Chrome(executable_path="drivers/chromedriver.exe")
     context.driver.maximize_window()
     # context.driver=webdriver.Chrome()
 
 
 @when("El usuario abre el sitio {site}")
-def openSauceDemo(context, site):
+def opensaucedemo(context, site):
     context.driver.get(site)
 
 
 @then('El usuario se inicia sesion con las credenciales usuario {user} y contraseña {password}')
-def logIn(context, user, password):
+def login(context, user, password):
     context.driver.find_element("id", "user-name").click()
     context.driver.find_element("id", "user-name").send_keys(user)
     context.driver.find_element("id", "password").click()
@@ -26,22 +26,22 @@ def logIn(context, user, password):
 
 
 @given('El usuario agrega el primer producto al carrito')
-def addProduct(context):
+def addproduct(context):
     context.driver.find_element("id", "add-to-cart-sauce-labs-backpack").click()
 
 
 @when('El usuario ingresa al carrito')
-def goToCart(context):
+def gotocart(context):
     context.driver.find_element("xpath", "//div[@id='shopping_cart_container']/a/span").click()
 
 
 @when('El usuario ingresa a la opcion de checkout')
-def goToCheckout(context):
+def gotocheckout(context):
     context.driver.find_element("id", "checkout").click()
 
 
 @when('El usuario llena todos sus datos con nombre {fistName}, apellido {lastName}, codigo postal {postalCode}')
-def fillUserInformation(context, fistName, lastName, postalCode):
+def filluserinformation(context, fistName, lastName, postalCode):
     context.driver.find_element("id", "first-name").click()
     context.driver.find_element("id", "first-name").send_keys(fistName)
     context.driver.find_element("id", "last-name").click()
@@ -52,19 +52,19 @@ def fillUserInformation(context, fistName, lastName, postalCode):
 
 
 @when('El usuario finaliza la compra')
-def finishThePurchase(context):
+def finishthepurchase(context):
     context.driver.find_element("id", "finish").click()
 
 
 @then('El usuario verifica que la compra ha sido completada con exito')
-def verifySuccessPurchase(context):
+def verifysuccesspurchase(context):
     status = context.driver.find_element("xpath", "//div[@id='header_container']/div[2]/span").is_displayed()
     assert status is True
     #context.driver.close()
 
 
 @when('El usuario verifica que exista la QTY de un producto')
-def verifyProductQTY(context):
+def verifyproductqty(context):
     status = context.driver.find_element("xpath",
                                          "//div[@id='cart_contents_container']/div/div/div[3]/div").is_displayed()
     assert status is True
@@ -72,20 +72,20 @@ def verifyProductQTY(context):
 
 
 @then('El usuario selecciona la opcion de Remove')
-def removeProduct(context):
+def removeproduct(context):
     context.driver.find_element("id", "remove-sauce-labs-backpack").click()
 
 
 @then('El usuario verifica que ya no exista la QTY de un producto')
-def verifyProductQTYisNotDisplayed(context):
+def verifyproductqtyisnotdisplayed(context):
     assert stat is False
 
 
 @when('El usuario selecciona la opcion de Continue Shopping')
-def continueShopping(context):
+def continueshopping(context):
     context.driver.find_element("id", "continue-shopping").click()
 
 
 @when('El usuario agrega el segundo producto al carrito')
-def addSecondProduct(context):
+def addsecondproduct(context):
     context.driver.find_element("id", "add-to-cart-sauce-labs-bike-light").click()
--- features/steps/logOutsteps.py
+++ features/steps/logOutsteps.py
@@ -1,48 +1,44 @@
-import os
-
 from behave import *
 import comprarProductosteps
-from dotenv import load_dotenv
 
 
 @given('El usuario ingresa el navegador')
-def launchBrowser(context):
-    comprarProductosteps.launchBrowser(context)
+def launchbrowser(context):
+    comprarProductosteps.launchbrowser(context)
 
 
 @when('El usuario ingresa el sitio {site}')
-def openSauceDemo(context, site):
-    comprarProductosteps.openSauceDemo(context, site)
+def opensaucedemo(context, site):
+    comprarProductosteps.opensaucedemo(context, site)
 
 
 @then('El usuario se ingresa sesion con las credenciales usuario {user} y contraseña {password}')
-def logIn(context, user, password):
-    comprarProductosteps.logIn(context, user, password)
+def login(context, user, password):
+    comprarProductosteps.login(context, user, password)
 
 
 @then('El usuario a finalizado la compra de un producto')
-def prepareCasePurchase(context):
-    load_dotenv()
-    comprarProductosteps.addProduct(context)
-    comprarProductosteps.goToCart(context)
-    comprarProductosteps.goToCheckout(context)
-    comprarProductosteps.fillUserInformation(context, os.getenv('username'), os.getenv('lastName'), os.getenv('postalCode'))
-    comprarProductosteps.finishThePurchase(context)
+def preparecasepurchase(context):
+    comprarProductosteps.addproduct(context)
+    comprarProductosteps.gotocart(context)
+    comprarProductosteps.gotocheckout(context)
+    comprarProductosteps.filluserinformation(context, "Miguel", "Miranda", "0000")
+    comprarProductosteps.finishthepurchase(context)
 
 
 @then('El usuario selecciona el menu de opciones')
-def sideBarOption(context):
+def sidebaroption(context):
     context.driver.find_element("id", "react-burger-menu-btn").click()
 
 
 @then('El usuario selecciona la opcion de LogOut')
-def logOutOption(context):
+def logoutoption(context):
     context.driver.implicitly_wait(15)
     context.driver.find_element("id", "logout_sidebar_link").click()
 
 
 @then('El usuario verifica que se ha deslogueado')
-def verifyLogOut(context):
+def verifylogout(context):
     status = context.driver.find_element("id", "user-name").is_displayed()
     assert status is True
     context.driver.close()
